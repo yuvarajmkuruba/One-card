7318ccdd54b448b76ec05603f8a79d5fe3dd68b3
yuvarajmkuruba <yuvarajmkuruba@gmail.com>
2023-04-19 17:03:19 +0530

    king

---
+++ images/index.js
@@ -0,0 +1,13 @@
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+  })
+
+  sr.reveal('.vide1',{ delay:550, origin:'top' })
+  sr.reveal('.text2',{ delay:550, origin:'left' })
+  sr.reveal('.metal .left',{ delay:500, origin:'right' })
+  sr.reveal('.metal .right',{ delay:800, origin:'top' })
+
+  
+  
